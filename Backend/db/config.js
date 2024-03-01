eef7888432e053b6902141f3cd57e3abc0e2de9a
Hassan <malikhassanhu55@gmail.com>
2024-03-01 14:14:43 +0500

    Update

---
+++ Backend/db/config.js
@@ -0,0 +1,5 @@
+// mATK8Ksnb9h4hadl
+// malikhassanhu55
+
+const mongoose = require("mongoose");
+mongoose.connect("mongodb+srv://malikhassanhu55:mATK8Ksnb9h4hadl@library.mybuzb7.mongodb.net/?retryWrites=true&w=majority&appName=library")
---
+++ Backend/index.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const app =express();
+const port =5000;
+require("./db/config")
+let User = require("./db/user")
+
+app.get("/", (req,res)=>{
+    res.send("api is working...")
+})
+
+app.post("/signup" , async (req,res)=>{
+    let user = User(req.body);
+    let result = await user.save();
+
+    res.send(result)
+})
+
+app.listen(port , ()=>{
+    console.log("server is running on ", port);
+})
