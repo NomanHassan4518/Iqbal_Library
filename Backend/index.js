f201d8cf761ad7c8c38e6cb7f10eac6e83759259
Hassan <malikhassanhu55@gmail.com>
2024-03-03 14:44:03 +0500

    as

--- Backend/index.js
+++ Backend/index.js
@@ -3,6 +3,8 @@ const app = express();
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
+app.use(express.json())
+
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
