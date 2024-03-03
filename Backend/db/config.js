cbe21de55684b77b650a0571917c0793c944ac9d
Hassan <malikhassanhu55@gmail.com>
2024-03-03 14:04:48 +0500

    sd

--- Backend/db/config.js
+++ Backend/db/config.js
@@ -2,4 +2,4 @@
 // malikhassanhu55
 
 const mongoose = require("mongoose");
-mongoose.connect("mongodb+srv://malikhassanhu55:mATK8Ksnb9h4hadl@library.mybuzb7.mongodb.net/?retryWrites=true&w=majority&appName=library") 
+mongoose.connect("mongodb://malikhassanhu55:mATK8Ksnb9h4hadl@ac-dql59su-shard-00-00.mybuzb7.mongodb.net:27017,ac-dql59su-shard-00-01.mybuzb7.mongodb.net:27017,ac-dql59su-shard-00-02.mybuzb7.mongodb.net:27017/?ssl=true&replicaSet=atlas-14n3mo-shard-0&authSource=admin&retryWrites=true&w=majority&appName=library") 
